6f2c6b916f4709c325b97d2b193b54e12316158f
Hari Dileepkumar <Hari.Dileepkumar@neudesic.com>
2018-07-19 04:25:01 +0530

    Web Application 1

---
+++ WebApplication/Controllers/EventController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+  
+    [Route("api/Event")]
+    public class EventController : Controller
+    {
+        public List<EventModel> Events { get; set; } = new List<EventModel>()
+            {
+                new EventModel()
+        {
+                    Id = 1,
+                    Name = "TT",
+                    Category = "Non technical",
+                    Venue = "Hall"
+                },
+                new EventModel()
+        {
+                    Id = 2,
+                    Name = "Quiz",
+                   Category = "Technical",
+                    Venue = "Base Camp"
+                }
+    };
+        // GET: api/Events
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Events);
+        }
+
+        // GET: api/Events/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var events = Events.FirstOrDefault(e => e.Id == id);
+            if (events != null)
+                return Ok(events);
+            else
+                return NotFound();
+        }
+
+        // POST: api/Events
+        [HttpPost]
+        public IActionResult Post(EventModel employee)
+        {
+            Events.Add(employee);
+
+            return Ok(Events);
+        }
+
+        // PUT: api/Events/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication/Models/EventModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Venue { get; set; }
+    }
+}
